93e1002796e32e505e4bc00769e83932f6653a1a
dadulya <makagonova.dasha@mail.ru>
2026-06-16 00:13:34 +0700

    Rest API

---
+++ bank-products/src/main/java/com/star/bank_products/dto/RuleListResponse.java
@@ -0,0 +1,21 @@
+package com.star.bank_products.dto;
+
+import com.star.bank_products.entity.DynamicRuleEntity;
+
+import java.util.List;
+
+public class RuleListResponse {
+    private List<DynamicRuleEntity> data;
+
+    public RuleListResponse(List<DynamicRuleEntity> data) {
+        this.data = data;
+    }
+
+    public List<DynamicRuleEntity> getData() {
+        return data;
+    }
+
+    public void setData(List<DynamicRuleEntity> data) {
+        this.data = data;
+    }
+}
