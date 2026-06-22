dbc8991d1e00455ee1eeb0e34557841bd54c9a5e
kirill-daw <kirill-daw2002@yandex.ru>
2026-06-22 12:03:36 +0500

    feat: add /management/info endpoint with build info

--- src/main/java/com/star/bank_products/controller/ManagementController.java
+++ src/main/java/com/star/bank_products/controller/ManagementController.java
@@ -1,4 +1,27 @@
 package com.star.bank_products.controller;
 
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/management")
 public class ManagementController {
+
+    private final BuildProperties buildProperties;
+
+    public ManagementController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @GetMapping("/info")
+    public Map<String, String> info() {
+        return Map.of(
+                "name", buildProperties.getName(),
+                "version", buildProperties.getVersion()
+        );
+    }
 }
